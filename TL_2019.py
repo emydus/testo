c1d2869c30a3dc4e6a31fd997af4aacef4a12c45
ABCLockwood <43783817+ABCLockwood@users.noreply.github.com>
2019-02-25 19:44:23 +0000

    Addition not working not sure If ill continue with

--- TL_2019.py
+++ TL_2019.py
@@ -150,4 +150,58 @@ def pos_time_heatmap(values,vmin,vmax):
 pos_time_heatmap('avg_occupancy',0,50)
 #pos_time_heatmap('avg_headway')
 
+#%%
+from sklearn.cluster import AffinityPropagation
+from sklearn import metrics
+
+X=dframe.set_index(pd.DatetimeIndex(dframe['datetime']))
+X=X.between_time('8:00','9:00')
+#X=X.reset_index()
+X=X.loc[:,'speedlane_1':'flow_total']
+#botched way of removing unused lanes for all categories concisely 
+#Change thresh based off time interval till desired columns are produced
+X=X.dropna(axis=1,thresh=50000)
+X=X.dropna()
+print(X.columns)
+#%%
+
+af = AffinityPropagation(preference=-50).fit(X)
+cluster_centers_indices = af.cluster_centers_indices_
+labels = af.labels_
+
+
+n_clusters_ = len(cluster_centers_indices)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+print("Adjusted Rand Index: %0.3f"
+      % metrics.adjusted_rand_score(labels_true, labels))
+print("Adjusted Mutual Information: %0.3f"
+      % metrics.adjusted_mutual_info_score(labels_true, labels))
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(X, labels, metric='sqeuclidean'))
+
+# #############################################################################
+# Plot result
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+plt.close('all')
+plt.figure(1)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    class_members = labels == k
+    cluster_center = X[cluster_centers_indices[k]]
+    plt.plot(X[class_members, 0], X[class_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=14)
+    for x in X[class_members]:
+        plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
 
